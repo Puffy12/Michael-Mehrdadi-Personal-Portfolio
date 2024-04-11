78361eecc69e393d5e77a93fe3b92c6ee3138104
Michael M <113219708+Puffy12@users.noreply.github.com>
2024-04-11 16:14:37 -0700

    Test

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -6,6 +6,7 @@ import { validateString, getErrorMessage } from "@/lib/utils";
 import ContactFormEmail from "@/email/contact-form-email";
 
 const resend = new Resend("re_C1CuzsXK_AfyspR2pfmtJN3SWWXv5GDUD");
+console.log("TEST TEST TEST "+ process.env.API_URL);
 
 export const sendEmail = async (formData: FormData) => {
   const senderEmail = formData.get("senderEmail");
