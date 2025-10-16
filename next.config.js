9a27adda6893c31a782d1180e1a80db8b0a897ed
Michael M <113219708+Puffy12@users.noreply.github.com>
2025-10-16 09:53:09 -0700

    Config Updated

--- next.config.js
+++ next.config.js
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "images.unsplash.com",
-        },
-      ],
-    },
-    
-  };
-  
-  module.exports = nextConfig;
+  images: {
+    // ✅ Allows loading optimized images from Unsplash
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+    qualities: [75, 85, 90, 95, 100],
+  },
+};
+
+module.exports = nextConfig;
